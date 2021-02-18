fd63581ecef4783a1151a846b1353173652b22e1
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2021-02-18 16:54:20 +0530

    add parcel

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import * as React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { Typography } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import Time from "./components/time";
 import TimeTable from "./components/timetable";
 import Grid from "@material-ui/core/Grid";
 import Greet from "./components/greet";
 import Container from "@material-ui/core/Container";
-// import Search from "./components/search";
-// import TopSites from "./components/topSites";
+import Search from "./components/search";
+import TopSites from "./components/topSites";
 
 const App: React.FC = () => {
   return (
     <>
       <CssBaseline />
+
       <Box
         px={2}
         pt={2}
@@ -27,32 +29,32 @@ const App: React.FC = () => {
           <Time />
           <Greet />
         </Box>
-        {/* <Container maxWidth="sm">
+        <Container maxWidth="sm">
           <Search />
-        </Container> */}
+        </Container>
         <Container maxWidth="lg">
           <Box p={2}>
             <Grid container spacing={1}>
-              <Grid item xs={12} md={12}>
-                {/* <Typography variant="caption" color="textSecondary">
+              <Grid item xs={12} md={12} lg={7}>
+                <Typography variant="caption" color="textSecondary">
                   TimeTable
-                </Typography> */}
+                </Typography>
                 <TimeTable />
               </Grid>
-              {/* <Grid item xs={12} md={6}>
-                {/* <Typography variant="caption" color="textSecondary">
+              <Grid item xs={12} md={6} lg={5}>
+                <Typography variant="caption" color="textSecondary">
                   Top Sites
-                </Typography> *
+                </Typography>
                 <TopSites />
-              </Grid> */}
+              </Grid>
             </Grid>
           </Box>
         </Container>
-        {/* <Box display="flex" justifyContent="center">
+        <Box display="flex" justifyContent="center">
           <Typography variant="caption" color="textSecondary">
             Made By Arpit Bhalla
           </Typography>
-        </Box> */}
+        </Box>
       </Box>
     </>
   );
